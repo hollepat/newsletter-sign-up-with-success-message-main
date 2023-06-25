be7f43450150c43305bab3875fcf84a09bccacf9
Holler, Patrik <hollepat@fel.cvut.cz>
2023-06-25 22:27:25 +0200

    validity check of email

--- js/main.js
+++ js/main.js
@@ -3,21 +3,26 @@ function submitForm(event) {
     // prevent submission
     event.preventDefault()
 
-    const newsletterEl = document.querySelector('.newsletter')
-    const dialogEl = document.querySelector('.dialog')
     
-    newsletterEl.classList.toggle('active');
-    dialogEl.classList.toggle('active')
-
     // get email
     const inputEl = document.querySelector('#email-input')
     let email_address = inputEl.value
-
+    
+    // check email
+    if (!validateEmail(inputEl)) {
+        return
+    }
+    
     // change it in dialog success
     const textEl = document.querySelector('#show-email')
-    email_address = (email_address === '') ? 'ash@loremcompany.com' : email_address
     textEl.innerText = email_address
     
+    // switch windows content
+    const newsletterEl = document.querySelector('.newsletter')
+    const dialogEl = document.querySelector('.dialog')
+    newsletterEl.classList.toggle('active');
+    dialogEl.classList.toggle('active')
+
 }
 
 function dismissDialog() {
@@ -28,4 +33,20 @@ function dismissDialog() {
     dialogEl.classList.toggle('active')
     newsletterEl.classList.toggle('active');
 
+    // reset input
+    document.form1.email1.classList.remove('error')
+    document.form1.email1.value = ''
 }
+
+function validateEmail(input) {
+
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  
+    if (input.value !== '' && input.value.match(validRegex)) {  
+      return true
+    } else {  
+      document.form1.email1.focus()
+      input.classList.add('error')
+      return false
+    }
+  }
